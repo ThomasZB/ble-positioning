58bbf22e92c39ad192075b0befae4fa49f78701d
thomaszb <thomaszb@qq.com>
2022-04-14 18:17:40 +0800

    修改工程结构

--- build/image_preload.cmake
+++
@@ -1,9 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD mynrf52811 CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT d:/Code/Nordic/test3;d:/Code/Nordic/test3/lpuart;d:/Code/Nordic/test3/beacon;d:/Code/Nordic/test3/direction_finding_connectionless_tx;d:/Code/Nordic/test3/direction_finding_locator;d:/Code/Nordic/test3/direction_finding_central;d:/Code/Nordic/test3/peripheral_uart;d:/Code/Nordic/test3/hci_lpuart;d:/Code/Nordic/test3/hci_uart;d:/Code/Nordic/test3/zhpyer_direction_finding_connectionless_rx;d:/Code/Nordic/test3/zhpyer_direction_finding_connectionless_tx;C:/Users/Thomas/ncs/v1.9.1/nrf;C:/Users/Thomas/ncs/v1.9.1/zephyr CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE d:/Code/Nordic/test3/my_test1/prj.conf CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_OPTIMIZATIONS y CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_THREAD_INFO y CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON c:\Users\Thomas\ncs\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
